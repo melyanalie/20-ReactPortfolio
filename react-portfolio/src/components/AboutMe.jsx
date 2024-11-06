47e186aa2536c63bffc8589175c46a6c92c28467
melyanalie <melyana.lie86@gmail.com>
2024-11-06 15:56:11 -0500

    added components folder and js files

---
+++ react-portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about-me">
+      <h2>About Me</h2>
+      <img src="/assets/profile.jpg" alt="Profile" />
+      <p>Hello! I'm a passionate web developer skilled in React. I love creating dynamic web applications and learning new technologies.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
